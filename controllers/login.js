ef017342a5dd6886f7d471fc19cbe0da6e81b77e
Keith Swanson <3818627+Psycoder42@users.noreply.github.com>
2018-07-07 10:00:32 -0500

    log in/out complete

--- controllers/login.js
+++ controllers/login.js
@@ -1,18 +1,45 @@
 // Module dependencies
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 // Custom modules
+const members = require('../models/member.js');
+const pageUtils = require('../utils/pageUtils.js');
 const security = require('../utils/securityUtils.js');
+
+// DB interactivity
+const Member = members.getModel(mongoose.connection);
 
 // The actual log in (Create route)
 router.post('/', (req, res)=>{
-  res.send('Attempting to log in');
+  let name = pageUtils.cleanString(req.body.username);
+  let pass = pageUtils.cleanString(req.body.password);
+  Member.findOne({username: name}, (err, data)=>{
+    if (err) {
+      // Log error for debugging purposes
+      console.log(err.message);
+      res.render('public/login.ejs', {
+        isNew: false,
+        error: "Log in was unsuccessful. Please try again later."
+      });
+    } else if (!security.matchesHash(pass, data.password)) {
+      // Bad password
+      res.render('public/login.ejs', {
+        isNew: false,
+        error: "Incorrect username/password combination."
+      });
+    } else {
+      // log them in and send them to their page
+      req.session.curUser = data;
+      res.redirect('/members/'+name);
+    }
+  });
 });
 
 // The actual log out (Destroy route)
 router.delete('/', (req, res)=>{
-  // Log them out and send the client to the main page
+  // Log them out and send them to the main page
   req.session.destroy(()=>{
     res.redirect('/');
   });
